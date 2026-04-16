06c85c2c218acd23dade026cd8c5b5b409f6b4ec
Chia1104 <yuyuchia7423@gmail.com>
2026-04-17 03:12:37 +0800

    fix: external tanstack devtools

--- src/components/root-provider.tsx
+++ src/components/root-provider.tsx
@@ -1,22 +1,22 @@
-import { lazy } from "react";
+// import { lazy } from "react";
 
 import { HeadContent, Scripts } from "@tanstack/react-router";
 
-const TanStackDevtools = lazy(() =>
-  import("@tanstack/react-devtools").then((module) => ({
-    default: module.TanStackDevtools,
-  }))
-);
-const ReactQueryDevtoolsPanel = lazy(() =>
-  import("@tanstack/react-query-devtools").then((module) => ({
-    default: module.ReactQueryDevtoolsPanel,
-  }))
-);
-const TanStackRouterDevtoolsPanel = lazy(() =>
-  import("@tanstack/react-router-devtools").then((module) => ({
-    default: module.TanStackRouterDevtoolsPanel,
-  }))
-);
+// const TanStackDevtools = lazy(() =>
+//   import("@tanstack/react-devtools").then((module) => ({
+//     default: module.TanStackDevtools,
+//   }))
+// );
+// const ReactQueryDevtoolsPanel = lazy(() =>
+//   import("@tanstack/react-query-devtools").then((module) => ({
+//     default: module.ReactQueryDevtoolsPanel,
+//   }))
+// );
+// const TanStackRouterDevtoolsPanel = lazy(() =>
+//   import("@tanstack/react-router-devtools").then((module) => ({
+//     default: module.TanStackRouterDevtoolsPanel,
+//   }))
+// );
 
 export const RootProvider = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -26,21 +26,23 @@ export const RootProvider = ({ children }: { children: React.ReactNode }) => {
       </head>
       <body className="font-sans wrap-anywhere antialiased ">
         {children}
-        <TanStackDevtools
-          config={{
-            position: "bottom-left",
-          }}
-          plugins={[
-            {
-              name: "TanStack Query",
-              render: <ReactQueryDevtoolsPanel />,
-            },
-            {
-              name: "Tanstack Router",
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-          ]}
-        />
+        {/* {process.env.NODE_ENV === "development" && (
+          <TanStackDevtools
+            config={{
+              position: "bottom-left",
+            }}
+            plugins={[
+              {
+                name: "TanStack Query",
+                render: <ReactQueryDevtoolsPanel />,
+              },
+              {
+                name: "Tanstack Router",
+                render: <TanStackRouterDevtoolsPanel />,
+              },
+            ]}
+          />
+        )} */}
         <Scripts />
       </body>
     </html>
